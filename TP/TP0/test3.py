fcb818debef9357c4af2986b1b268114e66b706f
Vasilis Skarleas <51437323+vskarleas@users.noreply.github.com>
2024-10-19 12:18:17 +0200

    Update test3.py

--- TP/TP0/test3.py
+++ TP/TP0/test3.py
@@ -1,3 +1,9 @@
+# ############################################################################
+# file_input test4.py
+# Programmation Orienté Objet (POO) TP0 - Polytech Sorbonne - 2024/2025 - S7
+# Auteurs : Vasileios Filippos Skarleas, Yanis Sadoun - Tous droits réservés.
+# ############################################################################
+
 import exercise3
 from exercise2 import Vecteur
 from math import pi
